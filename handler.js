dbb28d87120815181aade5ccb3a178bfd614e0ee
Ryan LaClair <ryanlaclair@gmail.com>
2018-10-29 19:54:03 -0400

    added parsing from message body

--- handler.js
+++ handler.js
@@ -5,44 +5,23 @@ const ses = new aws.SES();
 
 const myEmail = process.env.EMAIL;
 
-function generateResponse(code, payload) {
-  return {
-    statusCode: code,
-    body: JSON.stringify(payload)
-  };
-}
-
-function generateError(code, err) {
-  console.log(err);
-  return {
-    statusCode: code,
-    body: JSON.stringify(err.message)
-  };
-}
-
 function generateEmailParams(body) {
-  // const { email, name, content } = JSON.parse(body);
-
-  // if (!(email && name && content)) {
-  //   throw new Error(
-  //     "Missing parameters! Make sure to add parameters 'email', 'name', 'content'."
-  //   );
-  // }
+  const { firstName, lastName, email, message } = JSON.parse(body);
 
   return {
     Source: myEmail,
     Destination: { ToAddresses: [myEmail] },
-    ReplyToAddresses: ['test@gmail.com'],
+    ReplyToAddresses: [email],
     Message: {
       Body: {
         Text: {
           Charset: 'UTF-8',
-          Data: 'TEST'
+          Data: `Message from ${firstName}: \n\n${message}`
         }
       },
       Subject: {
         Charset: 'UTF-8',
-        Data: 'TEST'
+        Data: `Contact from ${firstName} ${lastName}`
       }
     }
   };
@@ -52,8 +31,13 @@ module.exports.handleForm = async (event, context) => {
   try {
     const emailParams = generateEmailParams(event.body);
     const data = await ses.sendEmail(emailParams).promise();
-    return generateResponse(200, data);
+
+    return {
+      statusCode: 200
+    };
   } catch (err) {
-    return generateError(500, err);
+    return {
+      statusCode: 500
+    };
   }
 };
